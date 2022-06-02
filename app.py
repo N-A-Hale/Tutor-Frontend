a351d43085373f823d0d9f7856492ed7414373bb
Phuong Nguyen <honnsey@gmail.com>
2022-06-02 14:48:21 +0930

    return to normal version

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import streamlit as st
 
-# # Custom imports
-# from multipage import MultiPage
-# from pages import browse_jobs, recommend_jobs, classify_jobs
+# Custom imports
+from multipage import MultiPage
+from pages import browse_jobs, recommend_jobs, classify_jobs
 
-# # Create an instance of the app
-# app = MultiPage()
+# Create an instance of the app
+app = MultiPage()
 
-# # Title of the main page
-# st.title("From Data to $$$")
+# Title of the main page
+st.title("From Data to $$$")
 
-# # Add all your applications (pages) here
-# app.add_page("Job Search", browse_jobs.app)
-# app.add_page("Job Recommendation", recommend_jobs.app)
-# app.add_page("Classify Job", classify_jobs.app)
+# Add all your applications (pages) here
+app.add_page("Job Search", browse_jobs.app)
+app.add_page("Job Recommendation", recommend_jobs.app)
+app.add_page("Classify Job", classify_jobs.app)
 
 
-# # The main app
-# app.run()
+# The main app
+app.run()
 
 # test
-st.write("Hello World!")
+# st.write("Hello World!")
